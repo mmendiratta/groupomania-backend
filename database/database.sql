50af4d8264ceee1602230de81d314bd8617d1675
ManavMendiratta <Manav.Mendiratta@target.com>
2022-04-05 12:45:06 -0500

    updated feed table

--- database/database.sql
+++ database/database.sql
@@ -1,18 +1,22 @@
 CREATE TABLE account (
-    email VARCHAR(40) PRIMARY KEY,
+    email VARCHAR(40) UNIQUE NOT NULL,
     hashed_password CHAR(60) NOT NULL,
     first_name CHAR(20) NOT NULL,
     last_name CHAR(20) NOT NULL
 );
 
 CREATE TABLE post (
-    post_id VARCHAR(16) NOT NULL CONSTRAINT post_id_unique UNIQUE PRIMARY KEY,
-    FOREIGN KEY (email) REFERENCES account (email),
+    FOREIGN KEY (account_id) REFERENCES account (account_id),
     title VARCHAR(20) NOT NULL,
     text_body VARCHAR(1024) NOT NULL
 );
 
-CREATE TABLE feed (
-    FOREIGN KEY (email) REFERENCES account (email),
-    FOREIGN KEY (post_id) REFERENCES post (post_id),
+CREATE TABLE account_feed_posts (
+    account_id INT NOT NULL,
+    role_id INT NOT NULL,
+    viewed_date TIMESTAMP,
+    PRIMARY KEY (account_id, post_id),
+    FOREIGN KEY (account_id) REFERENCES account (id),
+    FOREIGN KEY (post_id) REFERENCES post (id),
+   
 )
